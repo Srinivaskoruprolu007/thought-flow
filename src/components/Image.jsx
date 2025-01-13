754ee8a0d17da20a2b984d8e1ba39c593839cba4
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-13 22:40:45 +0530

    add navbar responsive for all screens

---
+++ src/components/Image.jsx
@@ -0,0 +1,17 @@
+import { IKImage } from "imagekitio-react";
+
+// eslint-disable-next-line react/prop-types
+const Image = ({ src, className, w, h }) => {
+  return (
+    <IKImage
+      urlEndpoint={import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT}
+      path={src}
+      loading="lazy"
+      className={className}
+      width={w}
+      height={h}
+    />
+  );
+};
+
+export default Image;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,61 @@
-
+import { useState } from "react";
+import { X, Menu, LogIn } from "lucide-react";
+import Image from "./Image";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* LOGO */}
+      <div className="flex items-center gap-4">
+        <Image src="Logo.png" className="w-10 h-10" w={32} h={32} />
+        <span className="text-xl font-bold">ThoughFlow</span>
+      </div>
 
-export default Navbar
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        {/* Mobile button */}
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? <X /> : <Menu />}
+        </div>
+        {/* Mobile menu */}
+        <div
+          className={` w-full h-screen flex flex-col gap-8 font-medium text-xl items-center justify-center absolute top-16 ${
+            open ? "-right-0" : "-right-[100%]"
+          } transition-all ease-in-out duration-300`}
+        >
+          {/* to give links after installing react router dom */}
+          <a href="#">Home</a>
+          <a href="#">Trending</a>
+          <a href="#">Most Popular</a>
+          <a href="#">About</a>
+
+          <a href="#">
+            <button className="py-2 px-4 rounded-3xl bg-green-500 text-white">
+              Login <LogIn className="inline" />
+            </button>
+          </a>
+        </div>
+      </div>
+
+      {/* DESKTOP MENU */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="#">Home</a>
+        <a href="#">Trending</a>
+        <a href="#">Most Popular</a>
+        <a href="#">About</a>
+
+        <a href="#">
+          <button className="py-2 px-4 rounded-3xl bg-green-500 text-white">
+            Login <LogIn className="inline" />
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
